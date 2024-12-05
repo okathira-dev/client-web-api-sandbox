724813688032b701e6708bf5b1022a25d1183d2c
okathira <43328895+okathira@users.noreply.github.com>
2024-12-06 02:13:28 +0900

    feat: UIにアイコン追加

--- src/button-accordion-with-keyboard/App.tsx
+++ src/button-accordion-with-keyboard/App.tsx
@@ -4,6 +4,46 @@ import { ReedSwitch } from "./components/ReedSwitch";
 import { RegisterSwitch } from "./components/RegisterSwitch";
 import { Accordion } from "./components/Accordion";
 import { AudioInitializer } from "./components/AudioInitializer";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import TuneIcon from "@mui/icons-material/Tune";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import PianoIcon from "@mui/icons-material/Piano";
+import QueueMusicIcon from "@mui/icons-material/QueueMusic";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "8px",
+  width: "100%",
+  position: "relative",
+} as const;
+
+const iconContainerStyle = {
+  position: "absolute",
+  left: "-40px",
+} as const;
+
+const componentContainerStyle = {
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+} as const;
+
+type ComponentWithIconProps = {
+  Icon: React.ElementType;
+  Component: React.ElementType;
+};
+
+const ComponentWithIcon = ({ Icon, Component }: ComponentWithIconProps) => (
+  <div style={containerStyle}>
+    <div style={iconContainerStyle}>
+      <Icon />
+    </div>
+    <div style={componentContainerStyle}>
+      <Component />
+    </div>
+  </div>
+);
 
 export function App() {
   return (
@@ -20,11 +60,11 @@ export function App() {
           margin: "auto",
         }}
       >
-        <VolumeControl />
-        <ReedPitchControls />
-        <ReedSwitch />
-        <RegisterSwitch />
-        <Accordion />
+        <ComponentWithIcon Icon={VolumeUpIcon} Component={VolumeControl} />
+        <ComponentWithIcon Icon={TuneIcon} Component={ReedPitchControls} />
+        <ComponentWithIcon Icon={MusicNoteIcon} Component={ReedSwitch} />
+        <ComponentWithIcon Icon={QueueMusicIcon} Component={RegisterSwitch} />
+        <ComponentWithIcon Icon={PianoIcon} Component={Accordion} />
       </div>
     </AudioInitializer>
   );
